5c4c596a626e3dcb4af7f90e43c7e1c75cdb8771
bethwelmanasseh-hash <bethwelmanasseh@gmail.com>
2026-04-16 21:18:25 +0300

    #First change

--- week6_stack and queues.py
+++ week6_stack and queues.py
@@ -24,3 +24,55 @@ class stack:
 
 
 stackvalues = stack()
+stackvalues.push(10)
+stackvalues.push(20)
+stackvalues.push(30)
+stackvalues.push(40)
+stackvalues.push(50)
+stackvalues.push(60)
+
+stackvalues.display()
+
+print("Top element in the stack:", stackvalues.peek())
+print("Remove elements in the stack:", stackvalues.pop())
+stackvalues.display()
+
+
+#_____CLASS QUEUE _____
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self,item):
+        self.queue.append(item)
+        print(f"Enqueue: {item}")
+
+    def dequeue(self):
+       if  self.is_empty():
+            return "Queue is empty"
+       return self.queue.pop()
+
+    def frontelement(self):
+        if self.is_empty():
+            return "Queues is empty"
+        return self.queue[0]
+    
+    def is_empty(self):
+        return len(self.queue) == 0
+    
+    def display(self):
+        print(f"Queue:{self.queue}")
+
+Queuevalues = Queue()
+Queuevalues.enqueue(10)
+Queuevalues.enqueue(20)
+Queuevalues.enqueue(30)
+Queuevalues.enqueue(40)
+Queuevalues.enqueue(50)
+Queuevalues.enqueue(60)
+
+Queuevalues.display()
+
+print("Top element: ", Queuevalues.frontelement)
+print("Remove element:", Queuevalues.dequeue)
+Queuevalues.display()
